968c9331bd779db67044f64df5e02c74ce93f3d1
Glo-dio <glodififa@gmail.com>
2023-06-28 18:18:02 +0200

    6/10 (eau05.py en cours)

--- eau04.py
+++ eau04.py
@@ -1,7 +1,6 @@
 import sys
 
-args = sys.argv
-
+# fonctions utilisées
 def nb_premier(nb):
 	i = 2
 	if nb <= 1:
@@ -13,7 +12,7 @@ def nb_premier(nb):
 			i += 1
 	return 1
 
-def next_nb_premier(nb):
+def find_next_nb_premier(nb):
 	if nb_premier(nb) == 1:
 		return nb
 	nb += 1
@@ -21,16 +20,26 @@ def next_nb_premier(nb):
 		nb += 1
 	return nb
 
-is_nb_arg_correct = True if len(args) == 2 else False
-
 def is_args_valid():
 	if is_nb_arg_correct == False:
 		return False
-	if args[1].isdigit() == False:
+	if sys.argv[1].isdigit() == False:
 		return False
 
 
+# Partie 1 : Gestion d'erreur
+is_nb_arg_correct = True if len(sys.argv) == 2 else False
+
 if is_args_valid() == False:
 	print(-1)
-else:
-	print(next_nb_premier(int(args[1])))
+	exit()
+
+# Partie 2 : Parsing
+nombre = int(sys.argv[1])
+
+
+# Partie 3 : Résolution
+next_nb_premier = find_next_nb_premier(nombre)
+
+# Partie 4 : Affichage
+print(next_nb_premier)
