39ac194ce4b1d98f42391e17d57309b2a9ba5a07
Glodi home <glodi@glodi-ThinkPad-P14s-Gen-3.(none)>
2023-07-20 17:52:38 +0200

    py = 6/15 ; js = 3/15

--- eau02.js
+++ eau02.js
@@ -1,12 +1,22 @@
 // fonctions utilisées
 
-// let arg = 
+const argument = process.argv
+let len_args = argument.length - 1
 
-const len_args = console.log(process.argv)
-
-console.log(len_args)
+function reverse_args()
+{
+	while (len_args > 1)
+		console.log(argument[len_args--]);
+}
 
 // Partie 1 : Gestion d'erreur
+const is_number_arg_are_correct = len_args > 1;
+
 // Partie 2 : Parsing
 // Partie 3 : Résolution
+if (is_number_arg_are_correct === false)
+	console.log("error.");
+else
+	reverse_args();
+
 // Partie 4 : Affichage
