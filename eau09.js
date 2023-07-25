6b6f20d5398f2d681c3e76dc899311aff073543a
Glodi home <glodi@glodi-ThinkPad-P14s-Gen-3.(none)>
2023-07-26 00:38:25 +0200

    js = 9/15 (1 en cours)

---
+++ eau09.js
@@ -0,0 +1,45 @@
+// fonctions utilisées
+
+function min_max(first, second)
+{
+	let min = 0;
+	let max = 0;
+	let numbers = [];
+
+	if (first < second)
+		min = first, max = second;
+	else if (first > second)
+		min = second , max = first;
+
+	for (let i = min ; i < max; i++)
+		numbers.push(i);
+
+	return numbers;
+}
+
+function is_arg_valid()
+{
+	if (is_nb_arg_correct === false)
+		return false;
+	if (isNaN(process.argv[2]) == true || isNaN(process.argv[3]))
+		return false;
+}
+
+// Partie 1 : Gestion d'erreur
+is_nb_arg_correct = process.argv.length === 4;
+
+if (is_arg_valid() === false)
+{
+	console.log("error");
+	process.exit(1);
+}
+
+// Partie 2 : Parsing
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+// Partie 3 : Résolution
+const list = min_max(arg1, arg2).join(' ');
+
+// Partie 4 : Affichage
+console.log(list);
